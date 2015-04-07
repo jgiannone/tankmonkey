4bd536e1fa8389fed5dc64429ef109186ac5ab29
Scott Puleo <spuleo@zulily.com>
2015-04-07 14:12:45 -0700

    added stats for tanks

--- src/js/app.js
+++ src/js/app.js
@@ -6,21 +6,41 @@ $(function(){
   $('#player-input').keypress(function(e){
     var searchText = e.target.value;
     // hide grid and alert if user is typing
-    $('.alert-error').addClass('hidden');
-    $('.player-stats-grid').addClass('hidden');
+    hideStats();
     // on enter call api
     if(e.which === 13){
       playerSearch(searchText).then(function(data){
-        if(data.count > 0) {
+        if(data.count === 1) {
           $('#player-name-heading').text(searchText);
-        $('.player-stats-grid').removeClass('hidden');
-        }else{
+          // append the rows
+          data.stats.forEach(function(stat){ return addTankStatRow(stat); });
+
+          $('.player-stats-grid').removeClass('hidden');
+        }else if(data.count === 0){
           $('.alert-error').removeClass('hidden');
+        }else{
+          // TODO Show a list of users;
         }
 
       });
       e.preventDefault();
     }
   });
+
+  function addTankStatRow(tankData){
+    var el = '<tr><td>' + tankData.tier + '</td>';
+    el = el + '<td>' + tankData.name + '</td>';
+    el = el + '<td>' + tankData.wins + '</td>';
+    el = el + '<td>' + tankData.losses + '</td>';
+    el = el + '<td>' + tankData.percentage + '</td>';
+    el = el + '<td>' + tankData.mastery + '</td></tr>';
+    $('#tank-stats').append(el);
+  }
+
+  function hideStats(){
+    $('.alert-error').addClass('hidden');
+    $('.player-stats-grid').addClass('hidden');
+    $('#tank-stats').empty();
+  };
 
 });
--- src/js/lib/player-search.js
+++ src/js/lib/player-search.js
@@ -1,8 +1,44 @@
+var TANKS = [];
+
+// initialize tanks on startup
+$.get('https://api.worldoftanks.com/wot/encyclopedia/tanks/?application_id=demo').done(function(results){
+  TANKS = results.data;
+});
+
 module.exports = function(searchText){
+
   return $.get('https://api.worldoftanks.com/wot/account/list/?application_id=demo&search=' + searchText)
-  .done(function(result){
-      return result.data || {};
+  .then(function(result){
+
+      var playerObject = result.data || {count: 0};
+      // Here we have to make another call with the ID of the player.
+      if(playerObject[0].account_id){
+        return $.get('https://api.worldoftanks.com/wot/account/tanks/?application_id=demo&account_id=' + playerObject[0].account_id).then(function(result){
+          playerObject[0].stats = mapTanks(result.data[playerObject[0].account_id]);
+          playerObject[0].count = 1;
+          return playerObject[0];
+        })
+      }
+      //else{
+        //return playerObject;
+      //}
+
   }).fail(function(err){
     console.log(err);
   });
+
+  function mapTanks(playerTanks){
+    return playerTanks.map(function(tank){
+      var TANK = TANKS[tank.tank_id]
+      var wins = tank.statistics.wins
+      var losses = tank.statistics.battles - wins;
+      var percentage = wins / tank.statistics.battles * 100;
+      return {  tier: TANK.level,
+                name: TANK.name_i18n,
+                wins: wins,
+                losses: losses,
+                percentage: Math.round(10 * percentage) / 10,
+                mastery: tank.mark_of_mastery };
+    });
+  };
 };
